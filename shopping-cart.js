9d8731b8938abe6e53df8787017b3b979f74a81f
Tommy <tannewguy2018@gmail.com>
2023-05-04 11:01:51 +0700

    Add total cost calculation loop (still has bugs)

--- shopping-cart.js
+++ shopping-cart.js
@@ -7,20 +7,33 @@ function closeModal() {
     document.getElementById("cartModal").style.display = "none";
 }
 
+
 function addToCart(element) {
     let itemName = element.parentNode.parentNode.querySelector("td:first-child").textContent;
-    let itemPrice = element.parentNode.parentNode.querySelector("td:nth-child(2)").textContent;
-    const cartItems = document.querySelectorAll('ul li');
-    let totalPrice = document.querySelectorAll('div p');
-    console.log(totalPrice);
+    let itemPrice = parseFloat(element.parentNode.parentNode.querySelector("td:nth-child(2)").textContent.slice(1));
+    const cartItems = document.querySelectorAll('li');
+    let total = 0;
+    const cartTotal = document.getElementById('cartTotal');
+    
+    
+    
     console.log(itemName, itemPrice);
 
     // loop through cartitems and add each one by one
     for (let i = 0; i < cartItems.length; i++) {
         if (cartItems[i].textContent === 'Empty') {
-            cartItems[i].textContent = itemName;
+            cartItems[i].textContent = `${itemName} ${itemPrice}`
             break;
         }
     }
-   
+
+    cartItems.forEach(cartItem => {
+        if (cartItem.textContent !== 'Empty') {
+          total += itemPrice;
+        }
+      });
+      
+      cartTotal.textContent = `Total: $${total.toFixed(2)}`;
+    
 }
+
