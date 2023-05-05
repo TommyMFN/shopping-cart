910b48775428cde2c663688d376c4be33b8e769a
Tommy <tannewguy2018@gmail.com>
2023-05-05 10:46:38 +0700

    Add PlayWright

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,39 +1,45 @@
+// Declare two variables to be used in addToCart() function
+let storedItemPrice = '';
+let total = 0;
 
+// Define function to open cart modal
 function openModal() {
     document.getElementById("cartModal").style.display = "block";
 }
 
+// Define function to close cart modal
 function closeModal() {
     document.getElementById("cartModal").style.display = "none";
 }
 
-
+// Define function to add item to cart
 function addToCart(element) {
+    // Extract item name and price from table row of clicked add-to-cart button
     let itemName = element.parentNode.parentNode.querySelector("td:first-child").textContent;
-    let itemPrice = parseFloat(element.parentNode.parentNode.querySelector("td:nth-child(2)").textContent.slice(1));
-    const cartItems = document.querySelectorAll('li');
-    let total = 0;
+    let itemPriceStr = element.parentNode.parentNode.querySelector("td:nth-child(2)").textContent;
+    let itemPrice = parseFloat(itemPriceStr.slice(itemPriceStr.lastIndexOf('$') + 1));
+
+    // Hardcode cart items and total DOM elements for simplicity
+    const cartItems = [
+        document.getElementById("cartItem1"),
+        document.getElementById("cartItem2"),
+        document.getElementById("cartItem3")
+    ];
     const cartTotal = document.getElementById('cartTotal');
     
-    
-    
+    // Log item name and price for testing
     console.log(itemName, itemPrice);
-
-    // loop through cartitems and add each one by one
+    
+    // Add each item to empty space in cart
     for (let i = 0; i < cartItems.length; i++) {
         if (cartItems[i].textContent === 'Empty') {
-            cartItems[i].textContent = `${itemName} ${itemPrice}`
+            cartItems[i].textContent = `${itemName} ${itemPrice}`;
+            storedItemPrice = parseFloat(itemPrice); // Convert price string to number
             break;
-        }
+        } else if (cartItems[i].textContent !== 'Empty') {
+            total += storedItemPrice; // Add price of previously added item
+            cartTotal.textContent =  `Total: $${total}`; // Update total text
+        } 
+        
     }
-
-    cartItems.forEach(cartItem => {
-        if (cartItem.textContent !== 'Empty') {
-          total += itemPrice;
-        }
-      });
-      
-      cartTotal.textContent = `Total: $${total.toFixed(2)}`;
-    
 }
-
